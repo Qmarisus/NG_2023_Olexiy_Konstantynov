f67230e687dbbfcdeb9769e9db0f71ef51427135
OKonstantynov <konstantynov.olexiy@gmail.com>
2023-10-05 18:17:49 +0300

    Add second and third tasks

---
+++ Lection 1/Task 2/main.py
@@ -0,0 +1,4 @@
+print ('Введіть цілі числа для обчислення їх суми:')
+first_number = (int(input('Першее число: ')))
+second_number = (int(input('Друге число: ')))
+print ('Сума =',first_number+second_number)
