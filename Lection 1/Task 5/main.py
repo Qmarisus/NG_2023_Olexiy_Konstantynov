52fd1a7f9693b01d817aaf01197214307acc859f
OKonstantynov <konstantynov.olexiy@gmail.com>
2023-10-11 19:06:35 +0300

    added fifth task of first lection

--- Lection 1/Task 5/main.py
+++ Lection 1/Task 5/main.py
@@ -1,4 +1,4 @@
-print("Дано рівняння виду ax^2+bx+c=0. Введіть значення a,b,c.")
+print("Given equation of the species ax^2+bx+c=0. Enter the values a,b,c.")
 a=int(input ("a = "))
 b=int(input ("b = "))
 c=int(input ("c = "))
@@ -6,13 +6,13 @@ d= pow(b,2)-(4*a*c)
 if(d>0):
     x1=(-b-pow(d,0.5))/(2*a),
     x2=(-b+pow(d,0.5))/(2*a),
-    print("Корені рівняння: x1=",x1,"\n", "x2=",x2)
+    print("Roots of the equation: x1=",x1,"\n", "x2=",x2)
 elif(d==0):
     x=-b/(2*a),
-    print("Рівняння має єдиний корінь. Х=",x)
+    print("Root of the equation: Х=",x)
 else:
     x1= (-b-pow(abs(d),0.5)*1j)/(2*a),
     x2= (-b+pow(abs(d),0.5)*1j)/(2*a),
-    print("Розв'язки рівняння: x1=",x1,"\n", "x2=",x2)
+    print("Roots of the equation: x1=",x1,"\n", "x2=",x2)
 
     
